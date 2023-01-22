19e76bf123fe623d0442f73d9b390cb2dea3d6e5
Tony Dandino <tdandino1005@gmail.com>
2023-01-22 12:53:39 -0600

    started code for server.js

--- server.js
+++ server.js
@@ -5,3 +5,21 @@ const api = require('./routes/index.js');
 const PORT = process.env || 3001;
 
 const app = express();
+
+
+// asks express to create a route for every file in the 'public' folder and give it a '/' route
+app.use(express.static('public'));
+// sets up express app to handel data parser, middle wear created req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// routes to route files
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+
+// app listener - starts the server
+app.listen(PORT, () => {
+  console.log(`Server available at localhost${PORT}`);
+});
